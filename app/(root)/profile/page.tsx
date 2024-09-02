2ce0a694098e6e92f59158012c2f5d7b5d0dd66d
CDay98 <102760296+CDay98@users.noreply.github.com>
2024-09-02 09:29:53 -0400

    implement stripe

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,6 +1,8 @@
 import Collection from '@/components/shared/Collection'
 import { Button } from '@/components/ui/button'
 import { getEventsByUser } from '@/lib/actions/event.actions'
+import { getOrdersByUser } from '@/lib/actions/order.actions'
+import { IOrder } from '@/lib/database/models/order.model'
 import { auth } from '@clerk/nextjs/server'
 import Link from 'next/link'
 import React from 'react'
@@ -9,6 +11,8 @@ const ProfilePage = async () => {
     const { sessionClaims } = auth();
     const userId = sessionClaims?.userId as string;
 
+    const orders = await getOrdersByUser({ userId, page: 1 })
+    const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
     const organizedEvents = await getEventsByUser({ userId, page: 1 })
 
   return (
@@ -25,7 +29,7 @@ const ProfilePage = async () => {
         </div>
       </section> 
 
-      {/* <section className="wrapper my-8">
+      <section className="wrapper my-8">
         <Collection 
           data={orderedEvents}
           emptyTitle="No event tickets purchased yet"
@@ -36,7 +40,7 @@ const ProfilePage = async () => {
           urlParamName="ordersPage"
           totalPages={2}
         />
-      </section>  */}
+      </section> 
 
       {/* Events Organized */}
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
